460c27436254922190e111b8f3acbed6cdd2bcfc
AI Coding Mentor <t.kubera@reply.de>
2024-07-23 11:27:36 +0200

    Creating prompt for observability

--- src/agents/docs_agent.py
+++ src/agents/docs_agent.py
@@ -1,3 +1,7 @@
+"""
+TODO Berücksichtigen, dass GPT nun Json-Objekte zurückgibt. Es gibt einen 
+'Payload', sowie einen 'Reasoning' key.
+"""
 import ast
 import astor
 import re
@@ -22,9 +26,15 @@ class DocsAgent(Observable):
         self.file_retriever = FileRetriever(target_path)
         self.project_name = os.path.basename(target_path.rstrip(os.sep)) # Letzter Ordername vom target_path, z.B. './path/to/IIRA' -> 'IIRA'
 
+    def _add_observability(self, prompt):
+        obs_prompt = self._prompts.get_observability_prompt()
+        return "\n".join([prompt, obs_prompt])
+
     def _clean_list(self, lst):
         return [x for x in lst if x]
 
+    def _get_class_name(self):
+        return self.__class__.__name__
 
 class InCodeAgent(DocsAgent):
     def __init__(
@@ -77,6 +87,7 @@ class InCodeAgent(DocsAgent):
 
     def _document_method(self, file_path, method_name, class_name):
         prompt = self._prompts.get_document_method_prompt()
+        prompt = self._add_observability(prompt)
         with open(file_path, "r", encoding="utf-8") as file:
             code = file.read()
         return self._model.get_completion(
@@ -90,6 +101,7 @@ class InCodeAgent(DocsAgent):
     def _document_class(self, file_path, class_name):
         # Documentation purely based on docstrings in self.in_code_docs_resp
         prompt = self._prompts.get_document_class_prompt()
+        prompt = self._add_observability(prompt)
         for i in range(len(self.in_code_docs_responses[file_path])):
             if class_name in self.in_code_docs_responses[file_path][i]:
                 index = i
@@ -106,6 +118,7 @@ class InCodeAgent(DocsAgent):
 
     def _extract_classes(self, file_path):
         prompt = self._prompts.get_exract_classes_prompt()
+        prompt = self._add_observability(prompt)
         with open(file_path, "r", encoding="utf-8") as file:
             code = file.read()
 
@@ -123,6 +136,7 @@ class InCodeAgent(DocsAgent):
 
     def _extract_methods(self, file_path, class_name="global"):
         prompt = self._prompts.get_exract_methods_prompt()
+        prompt = self._add_observability(prompt)
         with open(file_path, "r", encoding="utf-8") as file:
             code = file.read()
         return self._clean_list(
@@ -158,6 +172,7 @@ class InCodeAgent(DocsAgent):
 
     def make_readme(self):
         prompt = self._prompts.get_readme_prompt()
+        prompt = self._add_observability(prompt)
         return self._model.get_completion(
             prompt.format(
                 programming_language=self._programming_language,
@@ -193,6 +208,7 @@ class SummaryContextAgent(DocsAgent):
         
         # Actually create the diagram using openai
         prompt = self._prompts.get_system_context_plantuml_prompt()
+        prompt = self._add_observability(prompt)
         response = self._model.get_completion(
             prompt.format(
                 system_context=self.system_context_summary,
@@ -207,6 +223,7 @@ class SummaryContextAgent(DocsAgent):
     
     def _find_context(self, file_path):
         prompt = self._prompts.get_system_context_prompt()
+        prompt = self._add_observability(prompt)
         with open(file_path, "r", encoding="utf-8") as file:
             file_content = file.read()
         return self._model.get_completion(
@@ -217,6 +234,7 @@ class SummaryContextAgent(DocsAgent):
 
     def _summarize_context(self):
         prompt = self._prompts.get_system_context_diagram_prompt()
+        prompt = self._add_observability(prompt)
         return self._model.get_completion(
             prompt.format(
                 summaries_of_file_analyses=self.system_context_responses
